40c9750c4d1a41e27595f9809b8bdac335de19b4
Jonathan Safer <jonathansafer2@gmail.com>
2022-12-01 14:32:03 -0500

    testing

--- src/main.py
+++ src/main.py
@@ -1,21 +1,23 @@
 import serial
+import connectSerial
 
-serialPort = serial.Serial(port = "/dev/tty.usbserial-DN042JG0", baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+print(connectSerial.serial_ports())
+
+#serialPort = serial.Serial(port = "/dev/tty.usbserial-DN042JG0", baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
 
 serialString = ""                           # Used to hold data coming over UART
 
+# while(1):
 
-while(1):
+#     # Wait until there is data waiting in the serial buffer
+#     if(serialPort.in_waiting > 0):
 
-    # Wait until there is data waiting in the serial buffer
-    if(serialPort.in_waiting > 0):
+#         # Read data out of the buffer until a carraige return / new line is found
+#         serialString = serialPort.readline()
 
-        # Read data out of the buffer until a carraige return / new line is found
-        serialString = serialPort.readline()
+#         # Print the contents of the serial data
+#         print(serialString.decode('Ascii'))
 
-        # Print the contents of the serial data
-        print(serialString.decode('Ascii'))
-
-        # Tell the device connected over the serial port that we recevied the data!
-        # The b at the beginning is used to indicate bytes!
-        serialPort.write(b"Thank you for sending data \r\n")
+#         # Tell the device connected over the serial port that we recevied the data!
+#         # The b at the beginning is used to indicate bytes!
+#         serialPort.write(b"Thank you for sending data \r\n")
